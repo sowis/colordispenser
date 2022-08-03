27fb8811da1f8e1847be5e2c5c38144f3a8f8305
진혁 박 <jh040597@naver.com>
2022-08-03 12:29:27 +0900

    비슷한 색 추천 기능 개발

--- source/colorDispenser_js/main.mjs
+++ source/colorDispenser_js/main.mjs
@@ -1,9 +1,2 @@
 import * as API from './API.mjs';
 import * as similar_color from './similar_color.mjs';
-
-const target = { r: 231, g: 25, b: 177 };
-console.log(target);
-const hsi = similar_color.rgb_to_hsi(target);
-console.log(hsi);
-const result = similar_color.hsi_to_rgb(hsi);
-console.log(result);
--- source/colorDispenser_js/similar_color.mjs
+++ source/colorDispenser_js/similar_color.mjs
@@ -2,7 +2,9 @@
 // s: 0~1
 // i: 0~255
 
-export function rgb_to_hsi(rgb) {
+const recomend_number = 6;
+
+function rgb_to_hsi(rgb) {
     const rgb_max = Math.max(rgb.r, rgb.g, rgb.b);
     const rgb_min = Math.min(rgb.r, rgb.g, rgb.b);
 
@@ -21,7 +23,7 @@ export function rgb_to_hsi(rgb) {
     return { h, s, i };
 }
 
-export function hsi_to_rgb(hsi) {
+function hsi_to_rgb(hsi) {
     if (hsi.h == 0) {
         const r = hsi.i + 2 * hsi.i * hsi.s;
         const g = hsi.i - hsi.i * hsi.s;
@@ -58,4 +60,72 @@ export function hsi_to_rgb(hsi) {
         const b = hsi.i + hsi.i * hsi.s * Math.cos((Math.PI / 180) * (hsi.h - 240)) / Math.cos((Math.PI / 180) * (300 - hsi.h));
         return { r, g, b };
     }
+}
+
+function similar_color(rgb) {
+    const hsi = rgb_to_hsi(rgb);
+    let hue_changes = [];
+    let saturation_changes = [];
+    let intensity_changes = [];
+
+    for (let i = 0; i < recomend_number; ++i) {
+        const new_hsi = { ...hsi };
+        new_hsi.h += (360 / (recomend_number + 1)) * (i + 1);
+        new_hsi.h %= 360;
+        hue_changes.push(hsi_to_rgb(new_hsi));
+    }
+
+    for (let i = 0; i < recomend_number; ++i) {
+        const new_hsi = { ...hsi };
+        new_hsi.s = (1 / (recomend_number - 1)) * i;
+        saturation_changes.push(hsi_to_rgb(new_hsi));
+    }
+
+    for (let i = 0; i < recomend_number; ++i) {
+        const new_hsi = { ...hsi };
+        new_hsi.i = (255 / (recomend_number - 1)) * i;
+        intensity_changes.push(hsi_to_rgb(new_hsi));
+    }
+
+    return { hue_changes, saturation_changes, intensity_changes };
+}
+
+export function create_color_chips(rgb) {
+    const similar_colors = similar_color(rgb);
+
+    const $hue_chips = document.createElement('div');
+    $hue_chips.classList.add('hue_chips');
+    for (const hue_change of similar_colors.hue_changes) {
+        const $hue_chip = document.createElement('div');
+        $hue_chip.classList.add('color_chip');
+        $hue_chip.style.backgroundColor = rgba(hue_change.r, hue_change.g, hue_change.b);
+        $hue_chips.appendChild($hue_chip);
+    }
+
+    const $saturation_chips = document.createElement('div');
+    $saturation_chips.classList.add('saturation_chips');
+    for (const saturation_change of similar_colors.saturation_changes) {
+        const $saturation_chip = document.createElement('div');
+        $saturation_chip.classList.add('color_chip');
+        $saturation_chip.style.backgroundColor = rgba(saturation_change.r, saturation_change.g, saturation_change.b);
+        $saturation_chips.appendChild($saturation_chip);
+    }
+
+    const $intensity_chips = document.createElement('div');
+    $intensity_chips.classList.add('intensity_chips');
+    for (const intensity_change of similar_colors.intensity_changes) {
+        const $intensity_chip = document.createElement('div');
+        $intensity_chip.classList.add('color_chip');
+        $intensity_chip.style.backgroundColor = rgba(intensity_change.r, intensity_change.g, intensity_change.b);
+        $intensity_chips.appendChild($intensity_chip);
+    }
+
+    const $color_chips = document.createElement('div');
+    $color_chips.classList.add('color_chips');
+
+    $color_chips.appendChild($hue_chips);
+    $color_chips.appendChild($saturation_chips);
+    $color_chips.appendChild($intensity_chips);
+
+    return $color_chips;
 }
