4fba1778c083a34d4b9af7ec34fd557b7c171187
진혁 박 <jh040597@naver.com>
2022-08-09 11:04:37 +0900

    footer api 임시 비활성화

--- source/colorDispenser_js/footer.mjs
+++ source/colorDispenser_js/footer.mjs
@@ -1,15 +1,5 @@
 import * as API from './API.mjs';
 
-async function get_total_throughput_size() {
-    const { result } = await fetch(API.rest_2);
-    return result;
-}
-
-async function get_total_throughtput_count() {
-    const { result } = await fetch(API.rest_3);
-    return result;
-}
-
 function byte_formatting(byte) {
     const units = ['byte', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB'];
     let unit_count = 0;
@@ -23,11 +13,11 @@ function byte_formatting(byte) {
 }
 
 function get_process_string() {
-    Promise.all([get_total_throughput_size(), get_total_throughtput_count()]).then(values => {
-        const byte_string = byte_formatting(values[0]);
-        const process_string = "총 " + byte_string + ", " + values[1] + " 개의 이미지 처리";
+    Promise.all([fetch(API.rest_2), fetch(API.rest_3)]).then(values => {
+        const byte_string = byte_formatting(+(values[0].result));
+        const process_string = "총 " + byte_string + ", " + values[1].result + " 개의 이미지 처리";
         document.querySelector('footer .process_size').textContent = process_string;
     });
 }
 
-get_process_string();
+//get_process_string();
