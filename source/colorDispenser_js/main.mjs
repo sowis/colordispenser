1f4c2d261bda7dd4e91e1fdf8084dca8e03d87fe
진혁 박 <jh040597@naver.com>
2022-08-21 03:23:15 +0900

    esc 누르면 메인페이지로 가게 함

--- source/colorDispenser_js/main.mjs
+++ source/colorDispenser_js/main.mjs
@@ -1,5 +1,13 @@
 import * as utilities from '/colorDispenser_js/utilities.mjs';
 
+const $body = document.querySelector('body');
+
 (function main() {
     utilities.goto_main_page();
+
+    $body.addEventListener('keydown', e=> {
+        if (e.code == "Escape") { // esc 누를시 메인페이지로 감
+            utilities.goto_main_page();
+        }
+    });
 })();
